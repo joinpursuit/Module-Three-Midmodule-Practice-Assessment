9855d22d24d682ad745f5e56411ae02bf7a60b83
Coreen Cooper <coreencooper@pursuit.org>
2021-04-13 21:07:45 -0400

    apply styling to layout

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,14 +3,14 @@ import React from "react";
 const Profile = (props) => {
     const { name, age, image, bio, gpa, id } = props;
     return (
-        <section key={id}>
-            <h4>{name}</h4>
-            <p>age: {age}</p>
-            <img src={image} alt="student"></img>
-            <p>short bio {bio}</p>
+        <li key={id}>
+            <h3>{name}</h3>
+            <p>Age: {age}</p>
+            <img src={image} alt="student" width='150'></img>
+            <p className="Bio">Bio: {bio}</p>
             <p>GPA: {gpa}</p>
             <button>Add To Class</button>
-        </section>
+        </li>
     )
 }
 
