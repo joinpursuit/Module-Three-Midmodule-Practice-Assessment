19948ef025a9a68c0868979bf5d3c438d868fae3
Obaid Rustemi <orustemi@gmail.com>
2021-04-18 20:23:38 -0400

    add student and roster complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Students from "./Students"
+import Roster from "./Roster"
+
+
+
 
 
 class App extends React.Component {
+  state = { newStudents: []}
+
+  addStudent = (student) => {
+    this.setState((prevState) => ({
+      newStudents: [...prevState.newStudents, student],
+    }));
+  };
+
   render() {
     return (
       <div class="App">
-        Hello World
+        
+      <Students addStudent={this.addStudent}/>
+      <Roster newStudents={this.state.newStudents}/>
       </div>
     );
   }
