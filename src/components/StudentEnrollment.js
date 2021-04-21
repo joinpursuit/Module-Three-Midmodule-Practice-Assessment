d6655d5ced88b3073c0c38dbc23ee6af34e768df
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-04-21 19:29:00 -0400

    first

---
+++ src/components/StudentEnrollment.js
@@ -0,0 +1,22 @@
+function StudentEnrollment(props) {
+    const { enrolled, classSize, averageEnrolledGPA } = props;
+    return (
+      <section className="Roster">
+        <h2>Enrolled</h2>
+        <ul>
+          {enrolled.map((student) => {
+            return (
+              <li key={student.id}>
+                {student.name}: {student.GPA}
+              </li>
+            );
+          })}
+        </ul>
+        <h3>Total Student Count: {classSize}</h3>
+        <h3>Average GPA: {averageEnrolledGPA}</h3>
+      </section>
+    );
+  }
+  
+  export default StudentEnrollment;
+  
